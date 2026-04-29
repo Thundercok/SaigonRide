a8a21afb43f115bfffe9c2f905e14f9979cd3975
Hài Dón <cathuys69@gmail.com>
2026-04-30 02:35:16 +0700

    update so it rún in railway íntead

--- Sprint4/SaigonRide.App/obj/Debug/net10.0/SaigonRide.App.AssemblyInfo.cs
+++ Sprint4/SaigonRide.App/obj/Debug/net10.0/SaigonRide.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaigonRide.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58226fa00af9d5ec243960745ff668627f13ef0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40865458e296603f970cfef67b954ad106d27ec1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaigonRide.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaigonRide.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
